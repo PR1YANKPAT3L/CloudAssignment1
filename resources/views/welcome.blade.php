f008076bf33f61c5d2695eca729fc6933095de2f
PR1YANKPAT3L <silentcoder>
2016-02-11 01:23:32 -0500

    Added Login Button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,41 +4,17 @@
         <title>Laravel</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <link rel="stylesheet" href="/css/main.css" media="screen" title="no title" charset="utf-8">
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+        <div class="MainContainer">
+            <div class="LoginContainer">
+                <ul>
+                    <li><a href="/facebook/authorize"><img src="imgs/Facebook Square.png" alt="" /></a></li>
+                    <li><a href="/facebook/authorize"><img src="imgs/Google plus square.png" alt="" /></a></li>
+                    <li><a href="/facebook/authorze"><img src="imgs/Linkedin square.png" alt="" /></a></li>
+                    <li><a href="/facebook/authorize"><img src="imgs/Twitter square.png" alt="" /></a></li>
+                </ul>
             </div>
         </div>
     </body>
