6f4b7381fb1791cc0365ff2f8aba869c70c1a02d
Ubuntu <ubuntu@ip-172-31-42-18.us-west-2.compute.internal>
2016-02-11 05:45:54 +0000

    Fixed Social Login

--- config/eloquent-oauth.php
+++ config/eloquent-oauth.php
@@ -9,7 +9,7 @@ return [
         'facebook' => [
             'client_id' => '1696886703929425',
             'client_secret' => 'ce9529556a4c068ce081e2e6b33ca391',
-            'redirect_uri' => 'http://gapapp.dev/dashboard',
+            'redirect_uri' => 'http://ec2-52-10-0-44.us-west-2.compute.amazonaws.com',
             'scope' => [],
         ],
         'google' => [
@@ -19,9 +19,9 @@ return [
             'scope' => [],
         ],
         'github' => [
-            'client_id' => '12345678',
-            'client_secret' => 'y0ur53cr374ppk3y',
-            'redirect_uri' => 'https://example.com/your/github/redirect',
+            'client_id' => 'd892334af06ec30567f3',
+            'client_secret' => '0b3d92560552a994d00d3943cca1507fc4f28f64',
+            'redirect_uri' => 'http://ec2-52-10-0-44.us-west-2.compute.amazonaws.com',
             'scope' => [],
         ],
         'linkedin' => [
